0d9d903262b35575b258feb90ebb2f0926f2b8a0
wdvqmj7377 <16353896+bhtri@users.noreply.github.com>
2022-12-12 17:23:00 +0900

    ✅ #8 Create Fancy BottomNavigationBar

---
+++ lib/representation/screen/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HoneScreen extends StatefulWidget {
+  const HoneScreen({super.key});
+
+  @override
+  State<HoneScreen> createState() => _HoneScreenState();
+}
+
+class _HoneScreenState extends State<HoneScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/representation/screen/main_app.dart
+++ lib/representation/screen/main_app.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import 'package:travel_app/core/constants/color_palette.dart';
+import 'package:travel_app/core/constants/dimension_constants.dart';
+import 'package:travel_app/representation/screen/home_screen.dart';
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
@@ -10,11 +15,52 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Main app'),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: IndexedStack(
+        index: _currentIndex,
+        children: [
+          const HoneScreen(),
+          Container(color: Colors.blue),
+          Container(color: Colors.brown),
+          Container(color: Colors.yellow),
+        ],
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        currentIndex: _currentIndex,
+        onTap: (int idx) {
+          setState(() {
+            _currentIndex = idx;
+          });
+        },
+        selectedItemColor: ColorPalette.primaryColor,
+        unselectedItemColor: ColorPalette.primaryColor.withOpacity(0.2),
+        margin: const EdgeInsets.symmetric(
+          horizontal: kMediumPadding,
+          vertical: kDefaultPadding,
+        ),
+        items: [
+          SalomonBottomBarItem(
+            icon: const Icon(FontAwesomeIcons.house),
+            title: const Text('Home'),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(FontAwesomeIcons.solidHeart),
+            title: const Text('Likes'),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(FontAwesomeIcons.briefcase),
+            title: const Text('Booking'),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(FontAwesomeIcons.solidUser),
+            title: const Text('Profile'),
+          ),
+        ],
       ),
     );
   }
